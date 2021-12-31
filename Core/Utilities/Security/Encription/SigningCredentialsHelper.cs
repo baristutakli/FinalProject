c352352fe1657dc6eabebf6850a559f2e136f881
baristutakli <baristutakli@hotmail.com>
2021-12-31 18:37:21 +0300

    Added JWT Helpers...

--- Core/Utilities/Security/Encription/SigningCredentialsHelper.cs
+++ Core/Utilities/Security/Encription/SigningCredentialsHelper.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace Core.Utilities.Security.Encription
 {
-    public class SigningCredentialHelper
+    public class SigningCredentialsHelper
     {
         public static SigningCredentials CreateSigningCredentials(SecurityKey securityKey) {
             return new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha512Signature);
